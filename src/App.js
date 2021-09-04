fc9bdd8a039a4176b117ecb4089169b5ee818fe1
MMPgenave <mohammadmp14@gmail.com>
2021-09-04 15:29:20 +0430

    improving the css and adding some functionality

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import { Component } from "react";
 import data from "./data";
 import { Product } from "./Product";
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       name: "mohammad",
       info: data,
       removeItem: (I) => {
-        
-          console.log("remove function initialized!")
-       const newdata = data.filter(Index => {
-          return Index.id !== I
+        console.log("remove function initialized!");
+        const newdata = this.state.info.filter((Index) => {
+          return Index.id !== I;
         });
-        this.setState({...this.state, info: newdata });
-
-        }
+        this.setState({ ...this.state, info: newdata });
+      },
+      reloader:1
     };
   }
 
   render() {
     return (
       <>
-        <h1>Hi my name is {this.state.name}</h1>
-        <h2>these are my favorites</h2>
+        
         {<Product info={this.state.info} removefcn={this.state.removeItem}/>}
         
       </>
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,35 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 export const Product = (props) => {
-
   return (
     <>
-          
-          {props.info.map(I => {
-              return (
-                <div key={I.id} className="item_container">
-                  <h1>{I.name}</h1>
-                      <h1>{I.price}</h1>
-                      <button type="button" className="btn" onClick={()=>{props.removefcn(I.id)}}>delete</button>
-                </div>
-              );
-          })}
+      {props.info.map((I) => {
+        return (
+          <div key={I.id} className="item_container">
+            <h3>Product name : {I.name} course</h3>
+            <h3>Product price : {I.price}</h3>
+            <h4 className="numberOfProduct">{0}</h4>
+            <h4>{I.name}</h4>
+            <button type="button" className="increase">
+              +
+            </button>
+
+            <button type="button" className="decrease">
+              -
+            </button>
+
+            <button
+              type="button"
+              className="btn"
+              onClick={() => {
+                props.removefcn(I.id);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        );
+      })}
     </>
   );
 };
