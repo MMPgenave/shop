fe8b479a49980d6ef423a670b692d68fc0626bf3
MMPgenave <mohammadmp14@gmail.com>
2021-09-09 16:44:24 +0430

    ...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Lorem from "./LoremProject/Lorem";
+
 import App from "./App";
 
 ReactDOM.render(
   <>
-    <Lorem />
+    <App />
   </>,
   document.getElementById("root")
 );
